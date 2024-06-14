5f40e393b2e60a4245f359e3fbf310a5b0443943
Altan Dabaev <altdab97@gmail.com>
2024-06-13 21:37:12 -0400

    group project

---
+++ src/test/tests/BaseTest.java
@@ -0,0 +1,67 @@
+package tests;
+
+import com.aventstack.extentreports.ExtentTest;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import utils.ConfigReader;
+import utils.Report;
+
+import java.lang.reflect.Method;
+import java.util.concurrent.TimeUnit;
+
+public class BaseTest {
+    public WebDriver driver;
+    public Report report;
+    public ExtentTest testReport;
+
+    @BeforeMethod
+    public void setUpBase(Method method){
+        initializeDriver("chrome");
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.get(ConfigReader.readProperty("config.properties", "url"));
+        report.createTestReport(driver,method);
+        driver.get("https://www.saucedemo.com/");
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+
+        driver.findElement(By.id("login-button")).click();
+    }
+
+    @AfterMethod
+    public void tearDown(ITestResult result){
+        report.logTestResult(result);
+        driver.quit();
+    }
+
+    public void initializeDriver(String browserType){
+        switch (browserType.toLowerCase()){
+            case "chrome":
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+                break;
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+                break;
+            default:
+                System.out.println("Wrong browser type");
+        }
+    }
+    @BeforeSuite
+    public void setUpReport(){
+        report = new Report();
+        report.createReport();
+    }
+    @AfterSuite
+    public void endReport(){
+        report.closeReport();
+    }
+}
