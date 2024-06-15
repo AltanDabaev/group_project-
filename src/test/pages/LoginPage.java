10a4eaa77c4ab98d0108f42c523f908f9e91955e
HelloHelloAliia <aliiamarcus3@gmail.com>
2024-06-15 09:43:22 -0400

    LoginPageTest&LoginPage

--- src/test/pages/LoginPage.java
+++ src/test/pages/LoginPage.java
@@ -1,27 +1,43 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import tests.BaseTest;
+import utils.ConfigReader;
 
-public class LoginPage {
+import java.lang.reflect.Method;
+import java.util.concurrent.TimeUnit;
+
+public class LoginPage extends BaseTest {
     public LoginPage(WebDriver driver){
         PageFactory.initElements(driver, this);
     }
 
-//    @FindBy(name = "username")
-//    public WebElement usernameInputField;
-//
-//    @FindBy(name = "password")
-//    public WebElement passwordInputField;
-//
-//    @FindBy(tagName = "button")
-//    public WebElement loginBtn;
-//
-//    public void login(String username, String password){
-//        usernameInputField.sendKeys(username);
-//        passwordInputField.sendKeys(password);
-//        loginBtn.click();
-//    }
+    @FindBy(id = "user-name")
+    public WebElement usernameInputField;
+
+    @FindBy(id = "password")
+    public WebElement passwordInputField;
+
+    @FindBy(id = "login-button")
+    public WebElement loginBtn;
+    @FindBy(xpath = "//img[@src='/static/media/sl-404.168b1cce.jpg']")
+    public WebElement image;
+
+
+    public void Login (Method method){
+        initializeDriver("chrome");
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.get(ConfigReader.readProperty("config.properties", "url"));
+        report.createTestReport(driver,method);
+        driver.get("https://www.saucedemo.com/");
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+
+        driver.findElement(By.id("login-button")).click();
+    }
+
 }
--- src/test/tests/LoginPageTest.java
+++ src/test/tests/LoginPageTest.java
@@ -1,4 +1,49 @@
 package tests;
 
-public class LoginPageTest {
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pages.LoginPage;
+
+public class LoginPageTest extends BaseTest{
+    LoginPage page;
+
+    @BeforeMethod
+    public void setUp(){
+        page = new LoginPage(driver);
+        driver.get("https://www.saucedemo.com/");
+    }
+
+    @Test(testName = "US 301: Verify standard_user can login with right password")
+    public void test01(){
+        page.usernameInputField.sendKeys("standard_user");
+        page.passwordInputField.sendKeys("secret_sauce");
+
+        page.loginBtn.click();
+
+
+        Assert.assertTrue(driver.getCurrentUrl().contains("https://www.saucedemo.com/inventory.html"));
+    }
+    @Test(testName = "US 302: When locked out user tries to login with right password I would like to see an error message 'Epic sadface: Sorry, this user has been locked out.'")
+    public void test02(){
+        page.usernameInputField.sendKeys("locked_out_user");
+        page.passwordInputField.sendKeys("secret_sauce");
+
+        page.loginBtn.click();
+
+        Assert.assertTrue(driver.findElement(By.xpath("//h3[@data-test='error']")).getText().contains("Epic sadface: Sorry, this user has been locked out."));
+
+    }
+    @Test(testName = "US 303 - When problem_user logs in all items on homepage should display same images")
+    public void test03(){
+        page.usernameInputField.sendKeys("problem_user");
+        page.passwordInputField.sendKeys("secret_sauce");
+
+        page.loginBtn.click();
+
+        Assert.assertTrue(page.image.isDisplayed());
+
+    }
+
 }
