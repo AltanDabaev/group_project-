d5caf976c2d3394156155234813908fb80e9fb9d
Gulzhana Malikova <malikovag202@gmail.com>
2024-06-15 11:14:55 -0400

    Checkout and ShoppingCard

--- src/test/tests/CheckoutPageTest.java
+++ src/test/tests/CheckoutPageTest.java
@@ -1,4 +1,28 @@
 package tests;
 
-public class CheckoutPageTest {
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pages.CheckoutPage;
+import pages.HomePage;
+import utils.BrowserUtils;
+
+public class CheckoutPageTest extends BaseTest {
+    private CheckoutPage checkoutPage;
+
+    @BeforeMethod(alwaysRun = true)
+    public void setUp(){
+        checkoutPage = new CheckoutPage(driver);
+    }
+
+    @Test(testName = "US 308: Checkout Information")
+    public void US308() throws InterruptedException {
+        checkoutPage.shopCardConteiner.click();
+        checkoutPage.checkPage.click();
+        Assert.assertTrue(checkoutPage.firstName.isDisplayed());
+        Assert.assertTrue(checkoutPage.lastName.isDisplayed());
+        Assert.assertTrue(checkoutPage.postalCode.isDisplayed());
+    }
+
+
 }
--- src/test/tests/ShoppingCartPageTest.java
+++ src/test/tests/ShoppingCartPageTest.java
@@ -1,4 +1,28 @@
 package tests;
 
-public class ShoppingCartPageTest {
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pages.ShoppingCartPage;
+import utils.BrowserUtils;
+
+public class ShoppingCartPageTest extends BaseTest{
+    private ShoppingCartPage cartPage;
+
+    @BeforeMethod(alwaysRun = true)
+    public void setUp(){
+        cartPage = new ShoppingCartPage(driver);
+    }
+
+    @Test(testName = "US 309:  Add to cart")
+    public void US309() throws InterruptedException {
+        cartPage.addCard.click();
+        cartPage.shopCardConteiner.click();
+        Thread.sleep(1000);
+        Assert.assertTrue(cartPage.item.isDisplayed());
+
+
+
+
+    }
 }
