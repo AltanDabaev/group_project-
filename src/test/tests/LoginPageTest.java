1398349feb3c3fca190141c4cb6022832693546c
HelloHelloAliia <aliiamarcus3@gmail.com>
2024-06-16 08:57:30 -0400

    Aliia

--- src/test/tests/LoginPageTest.java
+++ src/test/tests/LoginPageTest.java
@@ -1,48 +1,64 @@
 package tests;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pages.LoginPage;
 
 public class LoginPageTest extends BaseTest{
+    private LoginPage loginPage;
+    private String expectedResult;
+
+    private WebElement expectedResult2;
     LoginPage page;
 
     @BeforeMethod
     public void setUp(){
-        page = new LoginPage(driver);
+        report.setAuthor("Aliia");
+        loginPage = new LoginPage(driver);
         driver.get("https://www.saucedemo.com/");
     }
 
-    @Test(testName = "US 301: Verify standard_user can login with right password")
+    @AfterMethod
+    public void end(){
+        report.logExpectedResult(expectedResult);
+
+    }
+
+
+    @Test(testName = "US 301: User Login Test", description = "Verify standard_user can login with right password")
     public void test01(){
-        page.usernameInputField.sendKeys("standard_user");
-        page.passwordInputField.sendKeys("secret_sauce");
+        loginPage.usernameInputField.sendKeys("standard_user");
+        loginPage.passwordInputField.sendKeys("secret_sauce");
 
-        page.loginBtn.click();
-
+        loginPage.loginBtn.click();
 
         Assert.assertTrue(driver.getCurrentUrl().contains("https://www.saucedemo.com/inventory.html"));
+        expectedResult = "https://www.saucedemo.com/inventory.html";
     }
-    @Test(testName = "US 302: When locked out user tries to login with right password I would like to see an error message 'Epic sadface: Sorry, this user has been locked out.'")
+    @Test(testName = "US 302: Locked out User Login Test", description = "When locked out user tries to login with right password I would like to see an error message 'Epic sadface: Sorry, this user has been locked out.'")
     public void test02(){
-        page.usernameInputField.sendKeys("locked_out_user");
-        page.passwordInputField.sendKeys("secret_sauce");
+        loginPage.usernameInputField.sendKeys("locked_out_user");
+        loginPage.passwordInputField.sendKeys("secret_sauce");
 
-        page.loginBtn.click();
+        loginPage.loginBtn.click();
 
-        Assert.assertTrue(driver.findElement(By.xpath("//h3[@data-test='error']")).getText().contains("Epic sadface: Sorry, this user has been locked out."));
-
+        Assert.assertTrue(loginPage.errorMessage.getText().contains("Epic sadface: Sorry, this user has been locked out."));
+        expectedResult = "Epic sadface: Sorry, this user has been locked out.";
     }
-    @Test(testName = "US 303 - When problem_user logs in all items on homepage should display same images")
+
+    @Test(testName = "US 303 - Problem User Login Test", description = "When problem_user logs in all items on homepage should display same images")
     public void test03(){
-        page.usernameInputField.sendKeys("problem_user");
-        page.passwordInputField.sendKeys("secret_sauce");
+        loginPage.usernameInputField.sendKeys("problem_user");
+        loginPage.passwordInputField.sendKeys("secret_sauce");
 
-        page.loginBtn.click();
+        loginPage.loginBtn.click();
 
-        Assert.assertTrue(page.image.isDisplayed());
+        Assert.assertTrue(loginPage.image.isDisplayed());
+//        expectedResult2 = loginPage.image;
 
     }
 
